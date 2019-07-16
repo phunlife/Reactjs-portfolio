e360546cab71dc730fddcf1f08ae072ba81f2c96
Andreas <andreas.aker91@gmail.com>
2019-07-16 09:00:32 +0100

    Added paragraph

--- src/App.js
+++ src/App.js
@@ -31,6 +31,13 @@ const App = () => {
             <h1>React with strapi</h1>
           </div>
         </div>
+        <div class="flex-row">
+          <div class="flex-small">
+          <p>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          </p>
+          </div>
+        </div>
       </div>
       {loggedIn && projectForm}
       <ListProjects handleEdit={handleEdit} loggedIn={loggedIn} />
