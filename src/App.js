3c11046d10f72c5537ec810ab0f31cda9a150540
Andreas <andreas.aker91@gmail.com>
2019-07-16 08:45:38 +0100

    Updated card max with

--- src/App.js
+++ src/App.js
@@ -1,45 +1,48 @@
-import React, {useState} from 'react';
-import ListProjects from './projects/ListProjects';
-import AddProjectForm from './projects/AddProjectForm';
-import EditProjectForm from './projects/EditProjectForm';
-import LoginForm from './admin/LoginForm'; 
-import './App.css';
+import React, { useState } from "react";
+import ListProjects from "./projects/ListProjects";
+import AddProjectForm from "./projects/AddProjectForm";
+import EditProjectForm from "./projects/EditProjectForm";
+import LoginForm from "./admin/LoginForm";
+import "./App.css";
 
 const App = () => {
+  const [loggedIn, setLoggedIn] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [currentProject, setCurrentProject] = useState({});
+  const handleEdit = project => {
+    setEdit(true);
+    setCurrentProject(project);
+  };
 
-    const [loggedIn, setLoggedIn] = useState(false);
-    const [edit, setEdit] = useState(false);
-    const [currentProject, setCurrentProject] = useState({});
-    const handleEdit = project => {
-      setEdit(true);
-      setCurrentProject(project);
-    }
+  let projectForm;
+  if (edit) {
+    projectForm = (
+      <EditProjectForm currentProject={currentProject} setEdit={setEdit} />
+    );
+  } else {
+    projectForm = <AddProjectForm />;
+  }
 
-    let projectForm;
-    if (edit){
-      projectForm = <EditProjectForm currentProject={currentProject} setEdit={setEdit} />;
-    }else{
-      projectForm = <AddProjectForm />
-    }
-
-    return (
-      <div class="App">
-        <div class="container space">
-          <div class="flex-row">
-            <div class="flex-small header-title"><h1>React with strapi</h1></div>
+  return (
+    <div class="App">
+      <div class="container space">
+        <div class="flex-row">
+          <div class="flex-small header-title">
+            <h1>React with strapi</h1>
           </div>
         </div>
-        { loggedIn &&
-          projectForm
-        }
-        <ListProjects handleEdit={handleEdit} loggedIn={loggedIn}/>
-    <div class="container footer">
+      </div>
+      {loggedIn && projectForm}
+      <ListProjects handleEdit={handleEdit} loggedIn={loggedIn} />
+      <div class="container footer">
         <div class="flex-row">
-          <div class="flex-small"><LoginForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} /></div>
+          <div class="flex-small">
+            <LoginForm loggedIn={loggedIn} setLoggedIn={setLoggedIn} />
+          </div>
         </div>
+      </div>
     </div>
-    </div>
-    );
-}
+  );
+};
 
 export default App;
--- src/projects/AddProjectForm.js
+++ src/projects/AddProjectForm.js
@@ -1,48 +1,69 @@
-import React,{useState} from 'react';
-import {postProject} from '../api/strapi';
+import React, { useState } from "react";
+import { postProject } from "../api/strapi";
 
 const AddProjectForm = props => {
+  const initProject = { title: "", link: "", description: "", img_link: "" };
+  const [project, setProject] = useState(initProject);
 
-	const initProject = {title: "", link: "", description: "", img_link: ""};
-	const [project, setProject] = useState(initProject);
+  const handleChange = e => {
+    const { name, value } = e.target;
+    setProject({ ...project, [name]: value });
+  };
 
-	const handleChange = e => {
-		const {name, value} = e.target
-		setProject({...project , [name]: value});
-	}
+  function handleSubmit(e) {
+    e.preventDefault();
+    postProject(project);
+    setProject(initProject);
+  }
 
-	function handleSubmit (e) {
-		e.preventDefault();
-		postProject(project)
-		setProject(initProject);
-	}
+  return (
+    <div class="container">
+      <form
+        onSubmit={e => {
+          handleSubmit(e);
+        }}
+      >
+        <div class="flex-row">
+          <div class="flex-small">
+            <label>Title</label>
+            <input
+              type="text"
+              name="title"
+              onChange={handleChange}
+              value={project.title}
+            />
+            <label>Link</label>
+            <input
+              type="text"
+              name="link"
+              onChange={handleChange}
+              value={project.link}
+            />
+            <label>Image link</label>
+            <input
+              type="text"
+              name="img_link"
+              onChange={handleChange}
+              value={project.img_link}
+            />
+          </div>
+          <div class="flex-small">
+            <label>Description</label>
+            <textarea
+              class="description"
+              name="description"
+              onChange={handleChange}
+              value={project.description}
+            />
 
-return(
-	<div class="container">
-	<form onSubmit={e => {
-		handleSubmit(e);
-	}}>
-		<div class="flex-row" >
-			<div class="flex-small">
-				<label>Title</label>
-				<input type="text" name="title" onChange={handleChange} value={project.title} />
-				<label>Link</label>
-				<input type="text" name="link" onChange={handleChange} value={project.link} />
-				<label>Image link</label>
-				<input type="text" name="img_link" onChange={handleChange} value={project.img_link} />
-			</div>
-			<div class="flex-small">
-				<label>Description</label>
-				<textarea class="description" name="description" onChange={handleChange} value={project.description} ></textarea>
-			
-				<button type="submit" class="float-right">Create</button>
-			
-		</div>
-
-		</div>
-	</form>
-	</div>
-	);
-}
+            <button type="submit" class="float-right">
+              Create
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default AddProjectForm;
